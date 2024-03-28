f7d17148b503aaaccd71f6b45ae5b62319438d8c
Jakub Pelák <jpelak@gmail.com>
2024-03-28 12:38:30 +0100

    type, css updates

--- src/lib/ArchiveSection.svelte
+++ src/lib/ArchiveSection.svelte
@@ -17,13 +17,13 @@ export let posts = $page.data.posts.slice(0, limit || Infinity)
   <div class="px1 py175 md:px2">
     <ul class="h5 rlh4">
       {#each posts as item (item.id)}
-        <li class="py025 row gap1"
-          ><a class="row items-center gap05" style="min-width: 0px;" href="/{item.id}"
-            >{#if item.pinned}<PinAngleFillIcon />
+        <li class="row gap1"
+          ><a class="py025 row gap05" style="min-width: 0px;" href="/{item.id}"
+            >{#if item.pinned}<PinAngleFillIcon class="my025" />
             {/if}<span class="block span1 ul ellipsis">{item.title}</span></a
           >
-          <span class="span1 border-bottom border-dashed border-gray border-alpha20" style="min-width: 1rem;" />
-          <span class="nowrap">{format(new Date(item.date), config.dateFormat)}</span></li
+          <span class="my025 span1 border-bottom border-dashed border-gray border-alpha20" style="min-width: 1rem;" />
+          <span class="my025 nowrap">{format(new Date(item.date), config.dateFormat)}</span></li
         >
       {/each}
     </ul>
